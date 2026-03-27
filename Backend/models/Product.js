68769051fad09917ae214cdc0ffebe9e2a5d78f2
Yashraj <yashrajkandhway9@gmail.com>
2026-03-27 21:40:04 +0530

    Final check for Product model

--- Backend/models/Product.js
+++ Backend/models/Product.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
-    name: String,
-    price: Number,
-    storeName: String,
-    distance: String,
-    category: String,
-    image: String
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    storeName: { type: String, required: true },
+    distance: { type: String, default: "1.0 km" },
+    category: { type: String },
+    image: { type: String }
 });
 
 module.exports = mongoose.model('Product', ProductSchema);
